319397352251bc42948da09480065fefe447473d
Masum Billah <149454290+masumgithu@users.noreply.github.com>
2024-09-19 22:10:56 +0600

    Initial commit

---
+++ practice8.py
@@ -0,0 +1,17 @@
+#love heart 😁 for none
+from functools import reduce
+from turtle import *
+color("red")
+begin_fill()
+left(50)
+forward(100)
+circle(40, 180)
+left(260)
+circle(40,180)
+forward(100)
+end_fill()
+BibiJan = ("This Is For You My BiBi__Jan")
+print(None)
+done()
+
+
